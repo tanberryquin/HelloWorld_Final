9f374685cb095d9f76dcf4b5064cca1dacd1f5b2
Michael Fernandez <meko.fernandez@gmail.com>
2016-02-24 21:23:04 +0800

    Added branch and a second line to display

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     
     public static void main(String[] args) {
 		System.out.println("Hello, Universe");
+		System.out.println("Welcome to my first Github exercise");
     }
 }
